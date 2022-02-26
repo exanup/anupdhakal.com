ad47cdfd49c82936ac944bcf32b8fb02521c11c7
Anup Dhakal <20139982+exanup@users.noreply.github.com>
2022-02-26 22:00:56 +0545

    WIP - google reCaptcha

--- src/templates/contact-page.js
+++ src/templates/contact-page.js
@@ -1,7 +1,9 @@
 /** @jsx jsx */
+import { useState } from 'react'
 import { jsx } from 'theme-ui'
 import { graphql } from 'gatsby'
 import { RiSendPlane2Line } from 'react-icons/ri'
+import ReCaptcha from '@pittica/gatsby-plugin-recaptcha'
 
 import Layout from '../components/layout'
 import Seo from '../components/seo'
@@ -25,10 +27,28 @@ export const pageQuery = graphql`
     }
 `
 
+/**
+ * https://www.google.com/recaptcha/admin/site/513914309/setup
+ * https://www.seancdavis.com/posts/how-to-use-netlify-forms-with-gatsby/
+ * https://www.gatsbyjs.com/plugins/@pittica/gatsby-plugin-recaptcha/?=recaptcha
+ * https://www.npmjs.com/package/react-recaptcha
+ */
+
 const Contact = ({ data }) => {
     const { markdownRemark, site } = data // data.markdownRemark holds your post data
     const { frontmatter, html } = markdownRemark
     const { description } = frontmatter
+
+    const [submitted, setSubmitted] = useState(false)
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        setSubmitted(true)
+    }
+
+    const handleVerify = (token) => {
+        console.log(token)
+    }
 
     return (
         <Layout className="contact-page" sx={contactStyles.contactPage}>
@@ -52,6 +72,7 @@ const Contact = ({ data }) => {
                     method="POST"
                     data-netlify="true"
                     data-netlify-honeypot="bot-field"
+                    onSubmit={handleSubmit}
                 >
                     <input type="hidden" name="form-name" value="contact" />
                     <p>
@@ -78,6 +99,12 @@ const Contact = ({ data }) => {
                             <textarea name="message" required></textarea>
                         </label>
                     </p>
+                    <ReCaptcha
+                        action="thanks"
+                        siteKey="6LfFtaEeAAAAAE3eeWLKDvORZzP5_h_3ZPGLEU20"
+                        onVerify={handleVerify}
+                        submitted={submitted}
+                    />
                     <p className="text-align-right">
                         <button
                             className="button"
